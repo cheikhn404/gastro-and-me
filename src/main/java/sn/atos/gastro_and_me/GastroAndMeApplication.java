c2ec9541e3df3ff448594fa304de30d26215090d
Cheikh Ndiaye <cheikhn414@gmail.com>
2022-12-05 13:20:04 +0000

    first commit

---
+++ src/main/java/sn/atos/gastro_and_me/GastroAndMeApplication.java
@@ -0,0 +1,13 @@
+package sn.atos.gastro_and_me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastroAndMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GastroAndMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sn/atos/gastro_and_me/GastroAndMeApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.atos.gastro_and_me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GastroAndMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
