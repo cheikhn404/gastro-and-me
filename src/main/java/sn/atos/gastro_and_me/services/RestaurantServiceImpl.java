e21924c82e274907096f06704ba7249a12975a98
Cheikh Ndiaye <cheikhn414@gmail.com>
2023-01-11 14:22:03 +0000

    dto restaurant added

--- src/main/java/sn/atos/gastro_and_me/services/RestaurantServiceImpl.java
+++ src/main/java/sn/atos/gastro_and_me/services/RestaurantServiceImpl.java
@@ -1,25 +1,37 @@
 package sn.atos.gastro_and_me.services;
 
 import java.util.List;
+
 import org.springframework.stereotype.Service;
+import sn.atos.gastro_and_me.dto.RestaurantDTO;
 import sn.atos.gastro_and_me.entities.RestaurantEntity;
+import sn.atos.gastro_and_me.mappers.RestaurantMapper;
 import sn.atos.gastro_and_me.repositories.RestaurantRepository;
 
 /**
- *
  * @author rubix
  */
 @Service
 public class RestaurantServiceImpl implements RestaurantService {
     private final RestaurantRepository restaurantRepository;
 
-    public RestaurantServiceImpl(RestaurantRepository restaurantRepository) {
+    private final RestaurantMapper restaurantMapper;
+
+    public RestaurantServiceImpl(RestaurantRepository restaurantRepository, RestaurantMapper restaurantMapper) {
         this.restaurantRepository = restaurantRepository;
+        this.restaurantMapper = restaurantMapper;
     }
 
     @Override
-    public RestaurantEntity addRestaurant(RestaurantEntity restaurant) {
-        return restaurantRepository.save(restaurant);
+    public RestaurantDTO addRestaurant(RestaurantDTO restaurantDTO) {
+        // Conversion automatique de dto en entité
+        RestaurantEntity restaurantEntity = restaurantMapper.toEntity(restaurantDTO);
+        // Sauvegarde du restaurant entité au niveau de la BDD
+        RestaurantEntity restaurantEntityCreated = restaurantRepository.save(restaurantEntity);
+        // Conversion automatique de entité en dto
+        RestaurantDTO restaurantDTO1 = restaurantMapper.toDto(restaurantEntityCreated);
+        // On retourne de dto crée
+        return restaurantDTO1;
     }
 
     @Override
@@ -29,17 +41,17 @@ public class RestaurantServiceImpl implements RestaurantService {
 
     @Override
     public List<RestaurantEntity> getRestaurants() {
-       return restaurantRepository.findAll();
+        return restaurantRepository.findAll();
     }
 
     @Override
     public RestaurantEntity getRestaurant(Long id) {
-        return  restaurantRepository.findById(id).get();
+        return restaurantRepository.findById(id).get();
     }
 
     @Override
     public void deleteRestaurant(Long id) {
         restaurantRepository.deleteById(id);
     }
-    
+
 }
